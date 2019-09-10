a68381e4ffdfc1c30b6545f49de360053f38b181
RAY2L <rlee00@stuy.edu>
2019-09-10 08:46:57 -0400

    fixed issue

--- KREWES.py
+++ KREWES.py
@@ -1,4 +1,4 @@
 import random
 
 KREWES = {'orpheus': [0, 1, 2], 'rex': [3, 4, 5], 'endymion': [6, 7, 8]}
-print(random.choice(KREWES[random.choice[KREWES.keys()]]))
+print(random.choice(KREWES[random.choice(KREWES.keys())]))
