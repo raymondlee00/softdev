822f004744eab9eef5e06afc17f8e78a90823661
ray. lee <rlee00@stuy.edu>
2019-09-11 21:53:20 -0400

    fixes

--- 02_randomizer/KRLL_leeR-liK.py
+++ 02_randomizer/KRLL_leeR-liK.py
@@ -1,4 +1,8 @@
 import random
 
-KREWES = {'orpheus': [0, 1, 2], 'rex': [3, 4, 5], 'endymion': [6, 7, 8]}
-print(random.choice(KREWES[random.choice(KREWES.keys())]))
+KREWES = {
+	'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+	'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+	'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+}
+print(random.choice(KREWES[random.choice(list(KREWES.keys()))]))
