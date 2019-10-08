b5163b87e1780b482e2a3d2243fc935f7795c571
ray. lee <rlee00@stuy.edu>
2019-10-08 08:31:10 -0400

    done

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -15,10 +15,10 @@ c = db.cursor()               #facilitate db ops
 
 
 
-with open('students.csv', newline='') as studentscsvfile:
-    studentscsvreader = csv.DictReader(studentscsvfile)
-    for row in studentscsvreader:
-        print(row['name'], row['age'], row['id'])
+# with open('students.csv', newline='') as studentscsvfile:
+#     studentscsvreader = csv.DictReader(studentscsvfile)
+#     for row in studentscsvreader:
+#         print(row['name'], row['age'], row['id'])
 
 #==========================================================
 
@@ -31,8 +31,7 @@ with open('courses.csv', newline='') as coursescsvfile:
     coursescsvreader = csv.DictReader(coursescsvfile)
     for row in coursescsvreader:
         print(row['code'], row['mark'], row['id'])
-    to_db = [(i['code'], i['mark'], i['id']) for i in coursescsvreader]
-    c.executemany("INSERT INTO courses (code, mark, id) VALUES (?, ?, ?);", to_db)
+        c.execute("INSERT INTO courses VALUES (\"{}\", \"{}\", \"{}\");".format(row['code'], row['mark'], row['id']))
 
 command = "CREATE table students(name TEXT, age INTEGER, id INTEGER)"        # test SQL stmt in sqlite3 shell, save as string
 c.execute(command) 
@@ -40,8 +39,7 @@ with open('students.csv', newline='') as studentscsvfile:
     studentscsvreader = csv.DictReader(studentscsvfile)
     for row in studentscsvreader:
         print(row['name'], row['age'], row['id'])
-    to_db = [(i['name'], i['age'], i['id']) for i in studentscsvreader]
-    c.executemany("INSERT INTO students (name, age, id) VALUES (?, ?, ?);", to_db)
+        c.execute("INSERT INTO students VALUES (\"{}\", \"{}\", \"{}\");".format(row['name'], row['age'], row['id']))
 
 #==========================================================
 
