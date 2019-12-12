c8d835927f19bf1a0521f56b46c5b054e9e236a5
ray. lee <rlee00@stuy.edu>
2019-12-12 08:56:18 -0500

    added remove

---
+++ 29_jsdom/dommy.js
@@ -0,0 +1,31 @@
+function fibonacci(i) {
+  return (
+    (1 / Math.sqrt(5)) *
+    (Math.pow((1 + Math.sqrt(5)) / 2, i) - Math.pow((1 - Math.sqrt(5)) / 2, i))
+  );
+}
+
+var changeHeading = function(e) {
+  var h = document.getElementById("h");
+  const res = e["target"]["innerText"];
+  h.innerHTML = res;
+  console.log(res);
+  // console.log(e);
+};
+
+var removeItem = function(e) {
+  console.log(e["target"]["firstChild"]);
+  e["target"].remove();
+};
+
+var lis = document.getElementsByTagName("li");
+
+for (var i = 0; i < lis.length; i++) {
+  lis[i].addEventListener("mouseover", (e) => {
+    changeHeading(e);
+  });
+  //   lis[i].addEventListener("mouseout");
+  lis[i].addEventListener("click", (e) => {
+    removeItem(e);
+  });
+}
