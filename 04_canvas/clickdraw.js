17d6d15df33f4378a2de20f736a8bea3ca8b85c3
ray. lee <rlee00@stuy.edu>
2020-02-05 15:36:29 -0500

    changed to 04_canvas

---
+++ 04_canvas/clickdraw.js
@@ -0,0 +1,48 @@
+// retrieve node in DOM via ID
+var canvas = document.getElementById("slate");
+
+// instantiate a CanvasRenderingContext2D object
+var ctx = canvas.getContext("2d");
+
+// invoke interface methods
+ctx.fillStyle = "#ff0000";
+
+var mode = "rectangle";
+
+function draw(event) {
+  // correct canvas bounds offset on mouse's x, y position
+  let rect = canvas.getBoundingClientRect();
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+
+  if (mode == "rectangle") {
+    // make it look like square is drawn from the center where user clicks
+    x -= 25;
+    y -= 25;
+    
+    ctx.fillRect(x, y, 50, 50);
+  } else {
+    var radius = 10;
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.lineWidth = 0;
+    ctx.strokeStyle = "#003300";
+    ctx.stroke();
+  }
+}
+
+function clearRect() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function switchMode() {
+  if (mode == "rectangle") {
+    mode = "dot";
+  } else {
+    mode = "rectangle";
+  }
+  console.log(mode);
+}
