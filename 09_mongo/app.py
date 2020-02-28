e9ce7c3d31a92b4d63e2c10d714f4ac36eb5d364
ray. lee <rlee00@stuy.edu>
2020-02-27 21:15:08 -0500

    formatted

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -4,7 +4,6 @@ from pprint import pprint
 
 client = MongoClient('localhost', 27017)
 db = client.test_database
-
 restaurants = db.restaurants
 
 # loads starting JSON dataset to the db
@@ -14,19 +13,14 @@ def ingest(f):
 
 result = restaurants.insert_many(ingest('primer-dataset.json'))
 
+
 # search all restaurants in in a given borough
 def search_borough(borough):
 	query = restaurants.find({'borough': borough})
 	return [restaurant for restaurant in query]
-	#result = list()
-	#for restaurant in query:
-	#	result.append(restaurant)
-	#return result
 
-# res = search_borough('Brooklyn')
-# pprint(res)
-# pprint(len(search_borough('Brooklyn')))
 # pprint(search_borough('Brooklyn')[:5])
+
 
 # search all restaurants in a given zipcode
 def search_zipcode(zipcode):
@@ -34,6 +28,7 @@ def search_zipcode(zipcode):
 	return [restaurant for restaurant in query]
 
 # pprint(search_zipcode('11221')[:5])
+
 
 # search all restaurants in a given zipcode and has a given grade
 def search_zipcode_grade(zipcode, grade):
@@ -45,9 +40,10 @@ def search_zipcode_grade(zipcode, grade):
 
 # pprint(search_zipcode_grade('11221', 'B')[:5])
 
+
 # search all restaurants in a given zipcode with a score below a given threshold
 def search_zipcode_score_threshold(zipcode, score_threshold):
 	query = restaurants.find({'address.zipcode': zipcode, 'grades': {'$elemMatch': {'score': {'$lt': score_threshold}}}})
 	return [restaurant for restaurant in query]
 
-pprint(search_zipcode_score_threshold('11221', 12)[:5])
+# pprint(search_zipcode_score_threshold('11221', 12)[:5])
