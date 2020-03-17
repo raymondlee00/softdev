88fafca39e3f314a64f63bd3a062cde9ebfb1697
ray. lee <rlee00@stuy.edu>
2020-03-16 20:55:07 -0400

    Update 11_mongoflask

--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -1,23 +1,33 @@
-# teamRM* - Michael Zhang and ray. lee.
-# SoftDev pd1
-# K11: Ay Mon Go Git It From Yer Flask
-# 2020-03-06
+# Team RM* -- ray. lee. and Michael Zhang
+# Softdev pd9
+# K11 -- Ay Mon Go Git It From Yer Flask
+# 2020-03-17
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
-from bson.json_util import loads
 from pprint import pprint
+from bson.json_util import loads
 
-client = MongoClient("localhost", 27017)
-db = client.RM
-meteors = db.meteors
+app = Flask(__name__)
+
+# setup Mongo database
+if meteors in db.list_meteorslection_names():
+    f = open("meteorites.json", "r")
+    rString = f.readlines()
+    t = loads(rString)
 
 # loads starting JSON dataset to the db
 def ingest(f):
     with open(f) as _f:
-        return loads(f'[{",".join(map(lambda s: s[:-1], _f))}]')
+        content = _f.read()
+        print(content)
+        _f.close()
+        dataset = json.loads(content)
+        return dataset[0]
 
-result = meteors.insert_many(ingest("meteorites.json"))
+
+result = meteors.insert_many(ingest("static/meteorites.json"))
+
 
 def filter_mass(mass):
     return list(meteors.find({"mass": mass}))
@@ -34,11 +44,11 @@ def filter_class(recclass):
 def filter_year(year):
     return list(meteors.find({"year": year}))
 
-app = Flask(__name__)
 
 @app.route("/")
 def root():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.debug = True
