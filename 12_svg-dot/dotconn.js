6a429a6fa9c4a73f13961a918dfc87092c2b7247
ray. lee <rlee00@stuy.edu>
2020-03-29 01:50:15 -0400

    Clean logic

--- 12_svg-dot/dotconn.js
+++ 12_svg-dot/dotconn.js
@@ -1,49 +1,57 @@
+"use strict";
+
 const pic = document.getElementById("vimage");
 const clearBtn = document.getElementById("clearBtn");
 const xmlns = "http://www.w3.org/2000/svg";
-var firstClick = true;
-var lastClickCoords = {
-    x: null,
-    y: null
+let firstClick = true;
+const lastClickCoords = {
+  x: 0,
+  y: 0
 };
 
-var draw = function(event) {
-    const mouseX = event.offsetX;
-    const mouseY = event.offsetY;
-    const mouseCoordsString = `${mouseX},${mouseY}`;
-    console.log(`Drawing a circle at (${mouseCoordsString})`);
-    const dot = document.createElementNS(xmlns, "circle");
-    dot.setAttribute("cx", mouseX);
-    dot.setAttribute("cy", mouseY);
-    dot.setAttribute("r", 10);
-    dot.setAttribute("fill", "lightgreen");
-    pic.appendChild(dot);
-    if (!firstClick) {
-	const line = document.createElementNS(xmlns, "line");
-	line.setAttribute("x1", lastClickCoords.x)
-	line.setAttribute("y1", lastClickCoords.y)
-	line.setAttribute("x2", mouseX)
-	line.setAttribute("y2", mouseY)
-	line.setAttribute("stroke", "black")
-	pic.appendChild(line);
-    }
-    lastClickCoords.x = mouseX
-    lastClickCoords.y = mouseY
-    if (firstClick) {
-	firstClick = false;
-    };
+const drawDot = (mouseX, mouseY) => {
+  const mouseCoordsString = `${mouseX},${mouseY}`;
+  console.log(`Drawing a circle at (${mouseCoordsString})`);
+  const dot = document.createElementNS(xmlns, "circle");
+  dot.setAttribute("cx", mouseX);
+  dot.setAttribute("cy", mouseY);
+  dot.setAttribute("r", 10);
+  dot.setAttribute("fill", "lightgreen");
+  pic.appendChild(dot);
 };
 
-var clear = function(event) {
-    event.preventDefault();
-    console.log("Clearing svg");
-    const blank = document.createElementNS(xmlns, "rect")
-    blank.setAttribute("height", 500);
-    blank.setAttribute("width", 500);
-    blank.setAttribute("fill", "white");
-    pic.appendChild(blank);
-    firstClick = true;
-}
+const drawLine = (mouseX, mouseY) => {
+  const line = document.createElementNS(xmlns, "line");
+  line.setAttribute("x1", lastClickCoords.x);
+  line.setAttribute("y1", lastClickCoords.y);
+  line.setAttribute("x2", mouseX);
+  line.setAttribute("y2", mouseY);
+  line.setAttribute("stroke", "black");
+  pic.appendChild(line);
+};
+
+const clear = () => {
+  pic.innerHTML = "";
+  firstClick = true;
+};
+
+const draw = event => {
+  const mouseX = event.offsetX;
+  const mouseY = event.offsetY;
+
+  drawDot(mouseX, mouseY);
+
+  if (firstClick) {
+    firstClick = false;
+  } else {
+    drawLine(mouseX, mouseY);
+    drawDot(lastClickCoords.x, lastClickCoords.y);
+    drawDot(mouseX, mouseY);
+  }
+
+  lastClickCoords.x = mouseX;
+  lastClickCoords.y = mouseY;
+};
 
 pic.addEventListener("mousedown", draw);
 clearBtn.addEventListener("click", clear);
