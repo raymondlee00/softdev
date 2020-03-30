47502f7d7846fcccf0be0da7ac3599c679e592ba
ray. lee <rlee00@stuy.edu>
2020-03-30 11:05:18 -0400

    Store onDotClick state correctly

--- 13_svg/dotconn.js
+++ 13_svg/dotconn.js
@@ -5,32 +5,40 @@ const clearBtn = document.getElementById("clearBtn");
 const xmlns = "http://www.w3.org/2000/svg";
 let onDotClick = false;
 
-const drawDot = (mouseX, mouseY) => {
-  const mouseCoordsString = `${mouseX},${mouseY}`;
-  console.log(`Drawing a circle at (${mouseCoordsString})`);
+const createDotElement = (mouseX, mouseY) => {
   const dot = document.createElementNS(xmlns, "circle");
-  dot.setAttribute("cx", mouseX);
-  dot.setAttribute("cy", mouseY);
-  dot.setAttribute("r", 30);
-  dot.setAttribute("fill", "lightgreen");
-  dot.setAttribute("data-num-clicks", 0);
-  dot.addEventListener("mouseup", e => mutateDot(e));
+  dot.setAttributeNS(null, "cx", mouseX);
+  dot.setAttributeNS(null, "cy", mouseY);
+  dot.setAttributeNS(null, "r", 30);
+  dot.setAttributeNS(null, "fill", "lightgreen");
+  dot.setAttributeNS(null, "data-num-clicks", 0);
+  dot.addEventListener("mouseover", () => (onDotClick = true));
+  dot.addEventListener("mouseout", () => (onDotClick = false));
+  dot.addEventListener("click", e => mutateDot(e));
+  return dot;
+};
+
+const drawDot = event => {
+  const mouseX = event.offsetX;
+  const mouseY = event.offsetY;
+
   if (!onDotClick) {
+    const dot = createDotElement(mouseX, mouseY);
     pic.appendChild(dot);
-  } 
+    const mouseCoordsString = `${mouseX},${mouseY}`;
+    console.log(`Drawing a circle at (${mouseCoordsString})`);
+  }
 };
 
 const mutateDot = event => {
-  onDotClick = true;
   const dot = event.target;
-  let numClicks = parseInt(dot.getAttribute("data-num-clicks"));
+  let numClicks = parseInt(dot.getAttributeNS(null, "data-num-clicks"));
   numClicks++;
   if (numClicks > 1) {
     console.log(numClicks);
   }
   dot.setAttribute("data-num-clicks", numClicks);
   console.log(dot);
-  onDotClick = false;
 };
 
 const clear = () => {
@@ -39,13 +47,8 @@ const clear = () => {
 };
 
 const draw = event => {
-  const mouseX = event.offsetX;
-  const mouseY = event.offsetY;
-
-  if (!onDotClick) {
-    drawDot(mouseX, mouseY);
-  }
+  drawDot(event);
 };
 
-pic.addEventListener("mousedown", draw);
+pic.addEventListener("click", draw);
 clearBtn.addEventListener("click", clear);
