fca958aaab34adb0d90172a630e9ad1506ec11da
ray. lee <rlee00@stuy.edu>
2020-03-30 12:31:39 -0400

    After green dot disappears, cannot draw bug

--- 13_svg/dotconn.js
+++ 13_svg/dotconn.js
@@ -3,7 +3,7 @@
 const pic = document.getElementById("vimage");
 const clearBtn = document.getElementById("clearBtn");
 const xmlns = "http://www.w3.org/2000/svg";
-let onDotClick = false;
+let onDot = false;
 
 const createDotElement = (mouseX, mouseY) => {
   const dot = document.createElementNS(xmlns, "circle");
@@ -12,22 +12,20 @@ const createDotElement = (mouseX, mouseY) => {
   dot.setAttributeNS(null, "r", 30);
   dot.setAttributeNS(null, "fill", "lightgreen");
   dot.setAttributeNS(null, "data-num-clicks", 0);
-  dot.addEventListener("mouseover", () => (onDotClick = true));
-  dot.addEventListener("mouseout", () => (onDotClick = false));
+  dot.addEventListener("mouseover", () => (onDot = true));
+  dot.addEventListener("mouseout", () => (onDot = false));
   dot.addEventListener("click", e => mutateDot(e));
   // console.log(dot);
   return dot;
 };
 
-const drawDot = event => {
-  const mouseX = event.offsetX;
-  const mouseY = event.offsetY;
-
-  if (!onDotClick) {
-    const dot = createDotElement(mouseX, mouseY);
+const drawDot = (dotX, dotY) => {
+  console.log("draw dot");
+  if (!onDot) {
+    const dot = createDotElement(dotX, dotY);
     pic.appendChild(dot);
-    const mouseCoordsString = `${mouseX},${mouseY}`;
-    console.log(`Drawing a circle at (${mouseCoordsString})`);
+    const dotCoordsString = `${dotX},${dotY}`;
+    console.log(`Drawing a circle at (${dotCoordsString})`);
   }
 };
 
@@ -38,31 +36,42 @@ const mutateDot = event => {
   // if (numClicks > 1) {
   //   console.log(numClicks);
   // }
+  // console.log("before mutation...", onDot);
   switch (numClicks) {
     case 1:
       dot.style.fill = "green";
       break;
     case 2:
+      // console.log("before random dot...", onDot);
       dot.parentNode.removeChild(dot);
-      const randomDot = createDotElement(
-        Math.random() * 400 + 50,
-        Math.random() * 400 + 50
-      );
+      const randomX = Math.random() * 400 + 50;
+      const randomY = Math.random() * 400 + 50;
+      const randomDot = createDotElement(randomX, randomY);
+      console.log("create random dot");
       pic.appendChild(randomDot);
+      // const jankDot = drawDot(event);
+      // jankDot.parentNode.removeChild(jankDot);
+      // onDot = false;
+      // console.log("after random dot...", onDot);
       break;
   }
-  dot.setAttribute("data-num-clicks", numClicks);
+  dot.setAttributeNS(null, "data-num-clicks", numClicks);
   console.log(dot);
 };
 
 const clear = () => {
   pic.innerHTML = "";
-  onDotClick = false;
+  onDot = false;
 };
 
 const draw = event => {
-  drawDot(event);
+  const mouseX = event.offsetX;
+  const mouseY = event.offsetY;
+  drawDot(mouseX, mouseY);
+  // console.log(onDot);
 };
 
 pic.addEventListener("click", draw);
 clearBtn.addEventListener("click", clear);
+
+setInterval(() => console.log(onDot), 1000);
